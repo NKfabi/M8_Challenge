9075a1642ea90169d68cd394d131ceb077798618
Fabian Riede <Fabian Riede@VITADMI-AC6S4M6>
2019-07-10 15:09:37 +0200

    update3

--- Team_BOT/src/de/vitbund/vitmaze/players/Feld.java
+++ Team_BOT/src/de/vitbund/vitmaze/players/Feld.java
@@ -2,7 +2,57 @@ package de.vitbund.vitmaze.players;
 
 public class Feld {
 
-	private String finish;
-	private int zaehler;
+	private int xWert;
+	private int yWert;
+	private String typ;
+	private boolean gesehen;
 	private boolean betreten;
+	private int zaehlerBetreten;
+	
+	public Feld(int yWert, int xWert, String typ, boolean gesehen, boolean betreten, int zaehlerBetreten) {
+		super();
+		this.yWert = yWert;
+		this.xWert = xWert;
+		this.typ = typ;
+		this.gesehen = gesehen;
+		this.betreten = betreten;
+		this.zaehlerBetreten = zaehlerBetreten;
+	}
+	
+	
+	public String getTyp() {
+		return typ;
+	}
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+	public boolean isGesehen() {
+		return gesehen;
+	}
+	public void setGesehen(boolean gesehen) {
+		this.gesehen = gesehen;
+	}
+	public boolean isBetreten() {
+		return betreten;
+	}
+	public void setBetreten(boolean betreten) {
+		this.betreten = betreten;
+	}
+	public int getZaehlerBetreten() {
+		return zaehlerBetreten;
+	}
+	public void setZaehlerBetreten(int zaehlerBetreten) {
+		this.zaehlerBetreten = zaehlerBetreten;
+	}
+
+	
+	public void feldBetreten() {
+		/**
+		 * TODO
+		 */
+//		if 
+		
+	}
+	
+	
 }
