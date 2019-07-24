aa07ccde913b3c928b30b3905e97002274392afa
Michaela Mölders <Michaela Mlders@VITADMI-7CR6LKH>
2019-07-24 10:04:53 +0200

    Blabla

--- Team_BOT/src/de/vitbund/vitmaze/players/FieldLvl1.java
+++ Team_BOT/src/de/vitbund/vitmaze/players/FieldLvl1.java
@@ -8,18 +8,18 @@ public class FieldLvl1 {
 	private int yWert;
 	private String status;
 	private boolean gesehen;
+	private boolean betreten;
 	private int zaehlerBetreten;
-
 	
 	
 	//Konstruktor
-	public FieldLvl1(int xWert, int yWert, String status, boolean gesehen, int zaehlerBetreten) {
+	public FieldLvl1(int xWert, int yWert, String status, boolean gesehen, boolean betreten, int zaehlerBetreten) {
 		this.xWert = xWert;
 		this.yWert = yWert;
 		this.status = status;
 		this.gesehen = gesehen;
+		this.betreten = betreten;
 		this.zaehlerBetreten = zaehlerBetreten;
-		
 	}
 	
 	
@@ -53,13 +53,19 @@ public class FieldLvl1 {
 		this.gesehen = gesehen;
 	}
 	
+	public boolean isBetreten() {
+		return betreten;
+	}
+	public void setBetreten(boolean betreten) {
+		this.betreten = betreten;
+	}
+	
 	public int getZaehlerBetreten() {
 		return zaehlerBetreten;
 	}
 	public void setZaehlerBetreten(int zaehlerBetreten) {
 		this.zaehlerBetreten = zaehlerBetreten;
 	}
-
 
 
 	
